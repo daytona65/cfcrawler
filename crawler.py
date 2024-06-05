115dd9f2ee3fc64f1af7dcc64966754d949d1ff2
daytona65 <niclim65@gmail.com>
2024-06-05 17:54:28 +0800

    Optimise script and collect invalid links as well

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from dotenv import load_dotenv
 from tqdm import tqdm
+import time
 
 load_dotenv()
 start_url = os.getenv('START_URL')
@@ -14,36 +15,39 @@ headers = {
     "Content-Type": "application/json"
 }
 
-# Function to check if the link is a valid Confluence / Google Doc page.
 def is_valid(url):
-    valid_domains=['confluence.shopee.io', 'docs.google.com/document/']
-    response = requests.get(url, headers=headers)
-    if 'docs.google.com/document/' not in url and (not (response.headers.get('content-type') == 'text/html;charset=UTF-8' or response.headers.get('content-type') == 'text/html; charset=utf-8') or response.status_code != 200):
-        invalid_links.append(url + " | " + response.headers.get('content-type') + " | " + str(response.status_code))
-        return []
-    for domain in valid_domains:
+    valid_substrings=['confluence.shopee.io', 'docs.google.com/document/']
+    invalid_substrings = ['#', '@', 'draftId=', '/plugins/', '/diffpagesbyversion/', '/spaces/', 'dopeopledirectorysearch', '/diffpages', 'pageworkflow', 'login.action?', '/users/']
+    for invalid in invalid_substrings:
+        if invalid in url:
+            return False
+    for domain in valid_substrings:
         if domain in url:
             return True
     return False
 
 # Returns list of links in a page
 def extract_links(url):
+    links = []
+    invalid_links = []
     response = requests.get(url, headers=headers)
+    content_type = response.headers.get('content-type').lower().replace(" ", "")
+    if 'docs.google.com/document/' not in url and (content_type != 'text/html;charset=utf-8' or response.status_code != 200):
+        invalid_links.append(url + " | " + response.headers.get('content-type') + " | " + str(response.status_code))
+        return links, invalid_links
     soup = BeautifulSoup(response.text, 'html.parser')
     links = list(filter(is_valid, [urljoin(url, link['href']) for link in soup.find_all('a', href=True)]))
-    return links
+    invalid_links += list(filter(lambda x : not is_valid(x), [urljoin(url, link['href']) for link in soup.find_all('a', href=True)]))
+    return links, invalid_links
 
 
 # Depth Limited Search
 def traverse_links(start_url, depth):
     visited = set()
+    invalid_visited = set()
     queue = [(start_url, 0)]
 
     while queue:
-        progress = ""
-        for i in range(len(queue)):
-            progress += "|"
-        print(f"Queue length: {len(queue)} " + progress)
         current_url, current_depth = queue.pop(0)
 
         if current_depth > depth:
@@ -51,18 +55,21 @@ def traverse_links(start_url, depth):
 
         if current_url in visited:
             continue
-
-        links = extract_links(current_url)
+        print(current_url)
+        links, invalid_links = extract_links(current_url)
         visited.add(current_url)
+        invalid_visited.update(invalid_links)
 
-        for link in links:
+        for i, link in enumerate(tqdm(links)):
             queue.append((link, current_depth + 1))
+            time.sleep(0.005)
 
-    return visited
+    return visited, invalid_visited
 
-invalid_links = []
-links = traverse_links(start_url, 2)
+links, invalid_links = traverse_links(start_url, 2)
 
+
+print("Writing to docs.txt and invaliddocs.txt.........")
 with open("docs.txt", "w") as file:
     to_write = '\n'.join(links)
     file.write(to_write)
